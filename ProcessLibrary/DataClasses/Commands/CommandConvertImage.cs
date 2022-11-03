3445e3a551f140931aee282758c8a589c0cf7cf1
MichaelI.Insberg <michael.insberg@4micro.de>
2022-11-03 19:00:36 +0100

    Start with image processing

---
+++ ProcessLibrary/DataClasses/Commands/CommandConvertImage.cs
@@ -0,0 +1,10 @@
+﻿using System.Drawing;
+
+namespace ProcessCommunication.ProcessLibrary.DataClasses.Commands
+{
+    public class CommandConvertImage : CommandBase
+    {
+        public Bitmap Bitmap { get; set; } 
+
+    }
+}
